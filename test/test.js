a8a75d6c6832e70ec90176f808beabe73b78daac
PupateYf <ice-4.ever@163.com>
2017-09-19 00:20:29 +0800

    feat: use jest to test the dir-ls.

--- test/test.js
+++ test/test.js
@@ -3,37 +3,44 @@
 let path = require('path');
 let ls = require('../index.js');
 
-describe('#dir-traversal', function () {
-    it('should traverse without error', function (done) {
-        var testPath = path.resolve('./test/parentsDir')
-        ls(testPath, {
-            errorHandle: function(err){
-                done(err)
-            },
-            allDone: function(){
-                done()
-            }
-        })
+test('dir-ls traverse without error', () => {
+    const done = jest.fn()
+    const testPath = path.resolve('./test/parentsDir')
+    ls(testPath, {
+        errorHandle: function (err) {
+            done(err)
+        },
+        allDone: function () {
+            done()
+        }
     })
-    it('should only traverse txt file', function(done){
-        var findRegExp = /txt/
-        var testPath = path.resolve('./test/parentsDir')
-        var fileList = []
-        ls(testPath, {
-            justFind: findRegExp,
-            errorHandle: function(err){
-                done(err)
-            },
-            fileCallback: function(res){
-                fileList.push(res.path)
-            },
-            allDone: function(){
-                let error = new Error();
-                fileList = fileList.filter(filePath => filePath.match(findRegExp))
-                fileList.length
-                ? done()
-                : done(error)
-            }
-        })
+    expect(done).toHaveBeenCalledTimes(0)
+})
+
+test('dir-ls should only traverse txt file', () => {
+    const findRegExp = /txt/
+    const testPath = path.resolve('./test/parentsDir')
+    let fileList = []
+
+    const done = jest.fn()
+    
+    ls(testPath, {
+        justFind: findRegExp,
+        errorHandle: function(err){
+            done(err)
+        },
+        fileCallback: function(res){
+            fileList.push(res.path)
+        },
+        allDone: function(){
+            let error = new Error();
+            fileList = fileList.filter(filePath => filePath.match(findRegExp))
+            fileList.length
+            ? done()
+            : done(error)
+        }
     })
+
+    expect(done).toHaveBeenCalledTimes(0)
+
 })
